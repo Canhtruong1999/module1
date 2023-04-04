51f0de6b62a63911347465646b3c0bf4b7f9e5dd
unknown <canhtruong2503@gmail.com>
2023-04-04 10:40:23 +0700

    3

--- sentence4.js
+++ sentence4.js
@@ -2,10 +2,10 @@ function staff(fullname, email) {
     this.fullname = fullname;
     this.email = email;
     this.setfullname = function (newfullname) {
-        this.fullname = newfullname
+        this.fullname = newfullname;
     }
     this.getfullname = function () {
-        return this.fullname;
+        return this.fullname
     }
     this.setemail = function (newemail) {
         this.email = newemail;
@@ -19,11 +19,11 @@ function staff(fullname, email) {
 
 }
 let david = new staff('david', 'david@gmail.com');
-console.log(david);
+// console.log(david);
 david.setfullname('David Do');
 david.setemail('david.do@gmail.com')
-console.log(david.tostring())
-console.log(david.getemail())
+// console.log(david.tostring())
+// console.log(david.getemail())
 let truong = new staff('truong', 'truong@gmail.com');
 let anh = new staff('anh', 'anh@gmail.com')
 let staffs = [david, truong, anh];
